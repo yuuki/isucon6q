4f69349263a9fd0459e0e9cc9bd88ee5bc0fbd6b
y_uuki <yuki.tsubo@gmail.com>
2016-09-17 16:10:54 +0900

    非整数のランダム値で置き換える

--- isuda.go
+++ isuda.go
@@ -14,7 +14,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 
@@ -321,16 +320,13 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	}
 	rows.Close()
 
-	keywords := make([]string, 0, 500)
-	for _, entry := range entries {
-		keywords = append(keywords, regexp.QuoteMeta(entry.Keyword))
-	}
-	re := regexp.MustCompile("("+strings.Join(keywords, "|")+")")
 	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
+	for _, entry := range entries {
+		kw := entry.Keyword
+		salt, _ := strrand.RandomString(`[a-zA-Z][あ-を][ア-ヲ]{20}`)
+		content = strings.Replace(content, kw, salt, -1)
+		kw2sha[kw] = salt
+	}
 	content = html.EscapeString(content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String()+"/keyword/" + pathURIEscape(kw))
